1edc21d83378ac8e079726c4d3ee79eb26f2ec4d
Xerna <ricardo.cerna2001@gmail.com>
2022-05-20 04:52:05 -0600

    creando la pestaña de movimientos realizados

--- main_view.php
+++ main_view.php
@@ -39,7 +39,6 @@ include('main.php')
         </div>
     </main>
     <hr class="mx-auto col-8 mt-5">
-
     <section id="options" class="container-fluid mt-4">
     <div class="row">
       <div class="col-10 mx-auto d-flex justify-content-between align-items-start">
@@ -62,10 +61,9 @@ include('main.php')
         <p class="fw-lighter mt-3 text-center" style="font-size: 0.8rem;">RETIRO SIN TARJETA</p>
         </div>
       </div>
-
       </div>
     </section>
-    <!-- JavaScript Bundle with Popper -->
+    <!-- JavaScript Bundle with Popper-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 
